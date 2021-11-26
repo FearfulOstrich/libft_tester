06fe0ff735488573e5ee5c3656ef5694a1e19434
Antoine ALLEON <aalleon@e1r7p17.42.fr>
2021-11-26 10:02:32 +0100

    include header file.

---
+++ includes/tests.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalleon <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 17:07:54 by aalleon           #+#    #+#             */
+/*   Updated: 2021/11/25 16:44:01 by aalleon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TESTS_H
+# define TESTS_H
+
+int	test_isalpha();
+int	test_isdigit();
+int	test_isalnum();
+int	test_isascii();
+int	test_isprint();
+int	test_strlen();
+int	test_toupper();
+int	test_tolower();
+int	test_strchr();
+int	test_strrchr();
+int	test_strncmp();
+int	test_atoi();
+int	test_memset();
+int	test_memcpy();
+int	test_memmove();
+int	test_memchr();
+#endif
